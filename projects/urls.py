841a13ffb5ed6b1aa6e299933662cfd11dce95c5
Praveen Raghav <praveenraghav04@gmail.com>
2021-01-31 03:31:24 +0530

    Updated homepage

--- projects/urls.py
+++ projects/urls.py
@@ -5,6 +5,7 @@ from projects import views
 
 app_name = 'projects'
 urlpatterns = [
-    path('', views.projects_home, name='projects-home')
+    path('', views.projects_home, name='projects-home'),
+    path('create_project', views.create_project, name='create-project' )
 ]
 
--- projects/views.py
+++ projects/views.py
@@ -10,3 +10,12 @@ def projects_home(request):
     context['projects'] = projects
     return render(request, 'projects/projects-home.html', context)
 
+
+def create_project(request):
+    context = {}
+    if request.method == 'POST':
+        return render(request, 'projects/added.html')
+
+    else:
+        return render(request, 'projects/create-project.html')
+
