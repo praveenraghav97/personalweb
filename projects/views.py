5a88d8375553f2376a7a1e255752addec9f6cc19
Praveen Raghav <praveenraghav04@gmail.com>
2021-01-31 04:23:30 +0530

    Updated Create Project

--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.contrib.auth.models import User, auth
 from .models import Project
 
 # Create your views here.
@@ -11,10 +13,24 @@ def projects_home(request):
     return render(request, 'projects/projects-home.html', context)
 
 
+@login_required (login_url='users/login/')
 def create_project(request):
     context = {}
     if request.method == 'POST':
-        return render(request, 'projects/added.html')
+        if User.username == 'praveenraghav04@gmail.com':
+            title = request.POST['title']
+            description = request.POST['description']
+
+            project = Project.objects.create(title=title, description=description)
+            project.save()
+            context = {"message": "Project added Successfully"}
+            return render(request, 'projects/added.html', context)
+        else:
+            context = {"message": "You are not allowed to add Project"}
+            return render(request, 'projects/create-project.html', context)
+
+
+
 
     else:
         return render(request, 'projects/create-project.html')
