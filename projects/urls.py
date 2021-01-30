2180b3f6464077c94122680549141efc049e2953
Praveen Raghav <praveenraghav04@gmail.com>
2021-01-31 03:40:48 +0530

    Updated Project Page 1

--- projects/urls.py
+++ projects/urls.py
@@ -6,6 +6,6 @@ from projects import views
 app_name = 'projects'
 urlpatterns = [
     path('', views.projects_home, name='projects-home'),
-    path('create_project', views.create_project, name='create-project' )
+    path('create_project/', views.create_project, name='create-project')
 ]
 
